1118af6890b815373dfdd54035a07e169a880626
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-12-07 07:20:39 -0800

    add'

--- Frontend/src/Store/AuthStore.js
+++ Frontend/src/Store/AuthStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 axios.default.withCredentials = true;
+const VITE_AUTH_API = "http://localhost:3000/api/auth"
 
 //fuction take setter:set as a argument
 export const useAuthStore = create((set) => ({
@@ -13,7 +14,7 @@ export const useAuthStore = create((set) => ({
     set({ isLoading: true, error: null });
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_AUTH_API}/signup`,
+        `${VITE_AUTH_API}/signup`,
         {
           email,
           password,
@@ -37,7 +38,7 @@ export const useAuthStore = create((set) => ({
     set({ isLoading: true, error: null });
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_AUTH_API}/verify-email`,
+        `${VITE_AUTH_API}/verify-email`,
         { code }
       );
       set({
@@ -57,7 +58,7 @@ export const useAuthStore = create((set) => ({
   checkAuthentication:async()=>{
     set({ isCheckingAuth: true, isLoading: true, error: null });
     try {
-        const response = await axios.get(`${import.meta.env.VITE_AUTH_API}/check-auth`,{withCredentials: true })
+        const response = await axios.get(`${VITE_AUTH_API}/check-auth`,{withCredentials: true })
         set({user:response.data.user,isAuthenticated:true,isCheckingAuth:false,isLoading: false})
     } catch (error) {
         set({error:null,isCheckingAuth:false,isAuthenticated:false,isLoading: false})
